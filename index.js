f0695bd625fc0f9fa10d6efdaeb56b15bcdb39ac
tanslisass <crepinstanislask@gmail.com>
2025-06-27 11:17:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,11 +36,4 @@ function startProject() {
 }
 
 startProject();
-require('http').createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Bot actif');
-}).listen(PORT, () => {
-  console.log(`[⚔️] Edo_TENSEI est à l'écoute sur le port ${PORT}`);
-});
-
 
